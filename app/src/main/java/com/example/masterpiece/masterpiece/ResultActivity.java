77cf66ed25b15836926e0d52dde80b6a7857a924
NorangBerry <hlnsud12@naver.com>
2018-07-31 17:07:57 +0900

    commit

--- app/src/main/java/com/example/masterpiece/masterpiece/ResultActivity.java
+++ app/src/main/java/com/example/masterpiece/masterpiece/ResultActivity.java
@@ -1,13 +1,72 @@
 package com.example.masterpiece.masterpiece;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ResultActivity extends AppCompatActivity {
 
+    Bitmap image;
+    String image_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+        image = intent.getParcelableExtra("image");
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageBitmap(image);
+        image_name = "k";
+    }
+    public void saveImage(View view){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,image_name+".jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            image.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void shareImage(View view){
+        try {
+            File file = new File(this.getExternalCacheDir(),image_name+".jpg");
+            FileOutputStream fOut = new FileOutputStream(file);
+            image.compress(Bitmap.CompressFormat.JPEG   , 100, fOut);
+            fOut.flush();
+            fOut.close();
+            file.setReadable(true, false);
+            final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            intent.setType("image/jpeg");
+            startActivity(Intent.createChooser(intent, "Share image via"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
